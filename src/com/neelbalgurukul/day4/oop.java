d8c31f76416c5ec66376fac4dac8f262f747b8e9
Prathamesh-Narayane <prathameshnarayane2802@gmail.com>
2023-07-20 11:20:34 +0530

    2

---
+++ src/com/neelbalgurukul/day4/oop.java
@@ -0,0 +1,52 @@
+package com.neelbalgurukul.day4;
+
+public class oop {
+	class Employee{
+	private String name;
+	private int id;
+	public long salary;
+	private String department;
+	
+	Employee(String name,int id,long salay,String department){
+		this.name=name;
+		this.id=id;
+		this.salary=salary;
+		this.department=department;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee a =new Employee("prashan",1,50000,"IT");
+		Employee b =new Employee("akash",2,55000,"IT");
+		Employee c =new Employee("rajesh",3,45000,"IT");
+		
+		System.out.print(a);
+		
+		
+		
+		if(a.getsalary()>b.getsalary()) {
+			 
+		
+			System.out.print("salary of a is higher");
+		}
+		else {
+			System.out.print("salary of b is higher");
+		}
+		if(a.getsalary()>c.getsalary) {
+			System.out.print("salary of a is higher");
+		}else {
+			System.out.print("salary of c is higher");
+		}
+		if(b.getsalary()>c.getsalary) {
+			System.out.print("salary of b is higher");
+		}
+		System.out.print("salary of c is higher");
+	}
+		
+		
+
+	}
+
+}
